9cdc8162a9249d6487ad0c09db268317765ac2b5
Domanski Daniel <nf959@o2.pl>
2019-08-24 14:20:57 +0200

    CCCCCCCCCCCCCCCCCCC

--- src/com/domanski/daniel/Main.java
+++ src/com/domanski/daniel/Main.java
@@ -3,7 +3,7 @@ package com.domanski.daniel;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Program start");
+        System.out.println("Program start!!!");
 
         System.out.println("Linia kodu");
 
